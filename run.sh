1fa886a60b332368030385c581f7c545249c50ff
nullobsi <me@nullob.si>
2020-11-18 12:03:50 -0800

    update run.sh to build go program

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
+#designed for my friend's macos machine
+echo "Downloading latest version..."
 git pull
+
+echo "Build dial renderer..."
+cd Canvas || exit
+GOOS=js GOARCH=wasm go build -o ../util/Canvas/go_build_Canvas_js
+cd ..
+
+echo "Run mod.ts..."
 ~/.deno/bin/deno run --unstable -A ./mod.ts
