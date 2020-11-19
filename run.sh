3a955b7273954dada8444fd1538e2dbf8e7fea61
nullobsi <me@nullob.si>
2020-11-18 17:51:24 -0800

    update script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 #designed for my friend's macos machine
 echo "Downloading latest version..."
-git pull
+git pull || exit
 
 echo "Build dial renderer..."
 cd Canvas || exit
-GOOS=js GOARCH=wasm go build -o ../util/Canvas/go_build_Canvas_js
+GOOS=js GOARCH=wasm go build -o ../util/Canvas/go_build_Canvas_js || exit
 cd ..
 
 echo "Run mod.ts..."
