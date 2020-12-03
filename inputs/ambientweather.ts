1256a608d05883fed2fcea75f14ae9dae50b367d
nullobsi <me@nullob.si>
2020-12-03 13:16:51 -0800

    ambient error handling

--- inputs/ambientweather.ts
+++ inputs/ambientweather.ts
@@ -9,7 +9,16 @@ const requestUrl = `https://api.ambientweather.net/v1/devices/?apiKey=${config.a
 const getData: DataInput = async function() {
     let response = await fetch(requestUrl);
     let json = await response.text();
-    let data = JSON.parse(json);
+    let data:any;
+    try {
+        data = JSON.parse(json);
+    } catch (e) {
+        throw new Error("Ambient didn't return valid JSON!")
+    }
+    let dev = data[0];
+    if (dev === undefined) {
+        throw new Error("Could not get devices from Ambient!")
+    }
     return data[0].lastData;
 }
 
