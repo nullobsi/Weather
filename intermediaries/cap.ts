4d6f8f11562656f347f2a0af0dc72e75bf9acb0f
nullobsi <me@nullob.si>
2020-12-03 08:52:33 -0800

    try to fix cap

--- intermediaries/cap.ts
+++ intermediaries/cap.ts
@@ -12,24 +12,27 @@ let inter: Intermediary = async function (opts, data, gradients, pipeline) {
     let opt = opts as CapOptions;
     let d = data[opt.oldField] as number;
     if (d === undefined) return;
-    let dstring = d.toString().replace(".", "")
-    let p = opt.maxDigits;
-    if (dstring.length > opt.maxDigits) {
-        while (dstring.length > opt.maxDigits) {
+    let res = cap(d, opt.maxDigits);
+    data[opt.fieldName] = res[0];
+
+    pipeline.datafields.filter(v => v.fieldName == opt.fieldName).forEach(v => v.perConfig.image !== undefined ? v.perConfig.image.presc = res[1] : null)
+}
+
+function cap(d: number, max: number){
+    let dstring = d.toString().replace(".","")
+    let p = max
+    if (dstring.length > max){
+        while (dstring.length > max) {
             d = Math.round(d*Math.pow(10,p))/Math.pow(10,p);
-            p--;
-            dstring = d.toString().replace(".","");
+            p--
+            dstring = d.toString().replace(".","")
         }
     }
-    data[opt.fieldName] = d;
-    let res = regex.exec(d.toString());
-    let nPresc:number;
-    if (res === null) nPresc = 0;
-    else {
-        nPresc = res[1].length
-    }
-
-    pipeline.datafields.filter(v => v.fieldName == opt.fieldName).forEach(v => v.perConfig.image !== undefined ? v.perConfig.image.presc = nPresc : null)
+    let res = /.+\.(.+)/.exec(d.toString())
+    let npresc: number;
+    if (res === null) npresc = 0;
+    else npresc = res[1].length
+    return [d, npresc];
 }
 
 export default inter;
