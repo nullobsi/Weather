6146f83f8a5b136cd9ed3b96217e91b746a4c38c
nullobsi <me@nullob.si>
2020-12-25 09:50:27 -0800

    upgrade discordeno

--- outputs/discord.ts
+++ outputs/discord.ts
@@ -1,4 +1,4 @@
-import * as Discord from "https://x.nest.land/Discordeno@9.0.1/mod.ts";
+import * as Discord from "https://x.nest.land/Discordeno@9.4.0/mod.ts";
 import getConfig from "../util/getConfig.ts";
 import tempToColor from "../util/tempToColor.ts";
 import DiscordRole from "../defs/DiscordRole.ts";
