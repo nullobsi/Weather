8d2ba8cbd2de3f66445b22efcff82530fd6fe6ba
nullobsi <me@nullob.si>
2021-03-10 16:52:46 -0800

    more optimizations

--- Canvas/MakeDial.go
+++ Canvas/MakeDial.go
@@ -65,14 +65,7 @@ func makeDial(c *gg.Context, dial js.Value, parsed *truetype.Font, gradient js.V
 	}
 
 	// casing gradient
-	casing := gg.NewLinearGradient(
-		x+ox+math.Cos(math.Pi-math.Pi/4)*radius,
-		y+oy+math.Sin(math.Pi-math.Pi/4)*radius,
-		x+ox+math.Cos(math.Pi)*radius,
-		y+oy+math.Sin(math.Pi)*radius,
-	)
-	casing.AddColorStop(0, color.Gray{0x66})
-	casing.AddColorStop(1, color.Gray{0xbb})
+	casing := getCasingGrad(x, ox, y, oy, radius)
 
 	// draw casing
 	c.SetStrokeStyle(casing)
@@ -81,6 +74,7 @@ func makeDial(c *gg.Context, dial js.Value, parsed *truetype.Font, gradient js.V
 	c.SetLineWidth(thickness * 1.3)
 	c.Stroke()
 
+	// gradient accesors
 	numPoints := gradient.Length()
 	getGrad := func(i int) (float64, color.Color) {
 		p := gradient.Index(i)
@@ -92,11 +86,13 @@ func makeDial(c *gg.Context, dial js.Value, parsed *truetype.Font, gradient js.V
 		return v
 	}
 
+	// find indexes
 	startIndex := util.FindLoIndex(startV, indexGrad, numPoints)
 	endIndex := util.FindLoIndex(endV, indexGrad, numPoints)
 
+	// cut gradient down, add keypoints
 	if startIndex != 0 {
-		nColor := util.GetColor(startIndex-1, startIndex, getGrad, startV)
+		nColor := util.InterpolateValue(startIndex-1, startIndex, getGrad, startV)
 		grad.AddColorStop(startV, nColor)
 	}
 
@@ -106,20 +102,21 @@ func makeDial(c *gg.Context, dial js.Value, parsed *truetype.Font, gradient js.V
 	}
 
 	if endIndex != numPoints-1 {
-		nColor := util.GetColor(endIndex, endIndex+1, getGrad, endV)
+		nColor := util.InterpolateValue(endIndex, endIndex+1, getGrad, endV)
 		grad.AddColorStop(endV, nColor)
 	}
 
 	grad.Normalize()
 
+	// draw gradient
 	c.DrawArc(x+ox, y+oy, radius, startAngle, endAngle)
 	c.SetLineWidth(thickness)
 	c.SetStrokeStyle(grad)
 	c.SetLineCap(gg.LineCapButt)
 	c.Stroke()
 	c.SetLineCap(gg.LineCapRound)
-	//draw texts
 
+	//draw texts
 	mult := 1.0
 	if dialType == 1 {
 		mult = 0.6
@@ -129,7 +126,7 @@ func makeDial(c *gg.Context, dial js.Value, parsed *truetype.Font, gradient js.V
 
 	c.SetColor(color.White)
 	c.SetFontFace(font)
-	//above c.DrawStringAnchored(dial.Get("displayName").String(), x+ox, y + oy - outerRadius,0.5,0)
+
 	if dialType != 1 {
 		c.DrawStringAnchored(dial.Get("displayName").String(), x+ox, y+oy-outerRadius/3, 0.5, 0.5)
 	} else if dialType == 1 {
@@ -146,27 +143,12 @@ func makeDial(c *gg.Context, dial js.Value, parsed *truetype.Font, gradient js.V
 	}
 	c.SetFontFace(font)
 
-	//fmt.Println(dial.Get("unit").String())
+	// check for value
 	if !dial.Get("value").IsNull() {
 		temp := dial.Get("value").Float()
-		var valueColor color.Color
+		var valueColor = util.GetColor(temp, getGrad, numPoints)
 
-		if temp < gradient.Index(0).Index(0).Float() {
-			valueColor, _ = colorx.ParseHexColor(gradient.Index(0).Index(1).String())
-		} else if temp > gradient.Index(numPoints-1).Index(0).Float() {
-			valueColor, _ = colorx.ParseHexColor(gradient.Index(numPoints - 1).Index(1).String())
-		} else {
-			lowI := numPoints - 1
-			for ; gradient.Index(lowI).Index(0).Float() >= temp && lowI > 0; lowI-- {
-			}
-
-			highI := lowI + 1
-
-			nColor := util.GetColor(lowI, highI, getGrad, temp)
-
-			valueColor = nColor
-		}
-
+		// get adjust value for angle calculations
 		tempAdj := (grad.transform(temp) - grad.min) / (grad.max - grad.min)
 		if tempAdj < 0 {
 			tempAdj = 0
@@ -174,6 +156,8 @@ func makeDial(c *gg.Context, dial js.Value, parsed *truetype.Font, gradient js.V
 			tempAdj = 1
 		}
 		valueAngle := startAngle + (endAngle-startAngle)*tempAdj
+
+		// draw value count
 		c.SetColor(valueColor)
 		valueStr := strconv.FormatFloat(math.Abs(temp), 'f', dial.Get("presc").Int(), 64)
 		if temp < 0 {
@@ -182,91 +166,14 @@ func makeDial(c *gg.Context, dial js.Value, parsed *truetype.Font, gradient js.V
 		if dialType == 1 {
 			valueStr += dial.Get("unit").String()
 		}
+
 		c.DrawStringAnchored(valueStr, x+ox, y+oy+radius*mult, 0.5, -0.2)
 
+		// draw indicator
 		if dialType == 0 {
-			//draw needle
-			needleWidth := outerRadius / 16
-			c.SetLineWidth(needleWidth / 2)
-			c.SetColor(color.Black)
-			c.DrawArc(x+ox, y+oy, needleWidth, 0, math.Pi*2)
-			c.Stroke()
-
-			c.MoveTo(math.Cos(valueAngle)*radius+x+ox, math.Sin(valueAngle)*radius+y+oy)
-			c.LineTo(math.Cos(valueAngle-math.Pi/2)*needleWidth/2+x+ox, math.Sin(valueAngle-math.Pi/2)*needleWidth/2+y+oy)
-			c.LineTo(math.Cos(valueAngle+math.Pi/2)*needleWidth/2+x+ox, math.Sin(valueAngle+math.Pi/2)*needleWidth/2+y+oy)
-			c.ClosePath()
-			c.Stroke()
-
-			c.MoveTo(math.Cos(valueAngle)*radius+x+ox, math.Sin(valueAngle)*radius+y+oy)
-			c.LineTo(x+ox, y+oy)
-			c.LineTo(math.Cos(valueAngle-math.Pi/2)*needleWidth/2+x+ox, math.Sin(valueAngle-math.Pi/2)*needleWidth/2+y+oy)
-			c.ClosePath()
-			c.SetColor(color.White)
-			c.Fill()
-
-			c.DrawArc(x+ox, y+oy, needleWidth, valueAngle, valueAngle-math.Pi)
-			c.Fill()
-
-			c.MoveTo(math.Cos(valueAngle)*radius+x+ox, math.Sin(valueAngle)*radius+y+oy)
-			c.LineTo(x+ox, y+oy)
-			c.LineTo(math.Cos(valueAngle+math.Pi/2)*needleWidth/2+x+ox, math.Sin(valueAngle+math.Pi/2)*needleWidth/2+y+oy)
-			c.ClosePath()
-			c.SetColor(color.Gray{180})
-			c.Fill()
-
-			c.DrawArc(x+ox, y+oy, needleWidth, valueAngle, valueAngle+math.Pi)
-			c.Fill()
-
-			c.DrawArc(x+ox, y+oy, needleWidth*0.4, 0, math.Pi*2)
-			c.SetColor(color.Black)
-			c.StrokePreserve()
-			c.SetColor(valueColor)
-			c.Fill()
+			drawNeedle(c, outerRadius/16, x+ox, y+oy, radius, valueAngle, valueColor)
 		} else if dialType == 1 {
-			//draw wind direction triangle needle
-			needleWidth := outerRadius / 16
-			needleAngle := math.Pi * 0.1
-			needleLength := thickness * 0.8
-			c.SetColor(color.Gray{0})
-			c.SetLineWidth(needleWidth / 2)
-
-			c.MoveTo(
-				x+ox+math.Cos(valueAngle)*(radius-needleLength),
-				y+oy+math.Sin(valueAngle)*(radius-needleLength),
-			)
-			c.LineTo(
-				x+ox+math.Cos(valueAngle+needleAngle/2)*(radius+needleLength),
-				y+oy+math.Sin(valueAngle+needleAngle/2)*(radius+needleLength),
-			)
-			c.LineTo(
-				x+ox+math.Cos(valueAngle)*(radius+needleLength),
-				y+oy+math.Sin(valueAngle)*(radius+needleLength),
-			)
-			c.LineTo(
-				x+ox+math.Cos(valueAngle-needleAngle/2)*(radius+needleLength),
-				y+oy+math.Sin(valueAngle-needleAngle/2)*(radius+needleLength),
-			)
-			c.ClosePath()
-			c.StrokePreserve()
-			c.SetColor(valueColor)
-			c.Fill()
-
-			c.MoveTo(
-				x+ox+math.Cos(valueAngle)*(radius-needleLength),
-				y+oy+math.Sin(valueAngle)*(radius-needleLength),
-			)
-			c.LineTo(
-				x+ox+math.Cos(valueAngle)*(radius+needleLength),
-				y+oy+math.Sin(valueAngle)*(radius+needleLength),
-			)
-			c.LineTo(
-				x+ox+math.Cos(valueAngle-needleAngle/2)*(radius+needleLength),
-				y+oy+math.Sin(valueAngle-needleAngle/2)*(radius+needleLength),
-			)
-			c.ClosePath()
-			c.SetColor(color.RGBA{0, 0, 0, 50})
-			c.Fill()
+			drawTriangle(c, outerRadius/16, x+ox, y+oy, thickness*0.8, valueAngle, valueColor, math.Pi*0.1, radius)
 		}
 
 	} else {
@@ -277,4 +184,108 @@ func makeDial(c *gg.Context, dial js.Value, parsed *truetype.Font, gradient js.V
 
 	c.Pop()
 
+}
+
+func getCasingGrad(x, ox, y, oy, r float64) gg.Pattern {
+	grad := gg.NewLinearGradient(
+		x+ox+math.Cos(math.Pi-math.Pi/4)*r,
+		y+oy+math.Sin(math.Pi-math.Pi/4)*r,
+		x+ox+math.Cos(math.Pi)*r,
+		y+oy+math.Sin(math.Pi)*r,
+	)
+	grad.AddColorStop(0, color.Gray{0x66})
+	grad.AddColorStop(1, color.Gray{0xbb})
+
+	return grad
+}
+
+func drawNeedle(c *gg.Context, width, x, y, length, angle float64, bulbColor color.Color) {
+	c.Push()
+	//draw bulb outline
+	c.SetLineWidth(width / 2)
+	c.SetColor(color.Black)
+	c.DrawArc(x, y, width, 0, math.Pi*2)
+	c.Stroke()
+
+	// draw needle outline
+	c.MoveTo(math.Cos(angle)*length+x, math.Sin(angle)*length+y)
+	c.LineTo(math.Cos(angle-math.Pi/2)*width/2+x, math.Sin(angle-math.Pi/2)*width/2+y)
+	c.LineTo(math.Cos(angle+math.Pi/2)*width/2+x, math.Sin(angle+math.Pi/2)*width/2+y)
+	c.ClosePath()
+	c.Stroke()
+
+	// draw white half of needle
+	c.MoveTo(math.Cos(angle)*length+x, math.Sin(angle)*length+y)
+	c.LineTo(x, y)
+	c.LineTo(math.Cos(angle-math.Pi/2)*width/2+x, math.Sin(angle-math.Pi/2)*width/2+y)
+	c.ClosePath()
+	c.SetColor(color.White)
+	c.Fill()
+
+	c.DrawArc(x, y, width, angle, angle-math.Pi)
+	c.Fill()
+
+	// draw grey half of needle
+	c.MoveTo(math.Cos(angle)*length+x, math.Sin(angle)*length+y)
+	c.LineTo(x, y)
+	c.LineTo(math.Cos(angle+math.Pi/2)*width/2+x, math.Sin(angle+math.Pi/2)*width/2+y)
+	c.ClosePath()
+	c.SetColor(color.Gray{180})
+	c.Fill()
+
+	c.DrawArc(x, y, width, angle, angle+math.Pi)
+	c.Fill()
+
+	// draw color bulb
+	c.DrawArc(x, y, width*0.4, 0, math.Pi*2)
+	c.SetColor(color.Black)
+	c.StrokePreserve()
+	c.SetColor(bulbColor)
+	c.Fill()
+	c.Pop()
+}
+
+func drawTriangle(c *gg.Context, width, x, y, length, valueAngle float64, bulbColor color.Color, angleWidth, radius float64) {
+	//draw wind direction triangle needle
+	c.Push()
+	c.SetColor(color.Gray{0})
+	c.SetLineWidth(width / 2)
+
+	c.MoveTo(
+		x+math.Cos(valueAngle)*(radius-length),
+		y+math.Sin(valueAngle)*(radius-length),
+	)
+	c.LineTo(
+		x+math.Cos(valueAngle+angleWidth/2)*(radius+length),
+		y+math.Sin(valueAngle+angleWidth/2)*(radius+length),
+	)
+	c.LineTo(
+		x+math.Cos(valueAngle)*(radius+length),
+		y+math.Sin(valueAngle)*(radius+length),
+	)
+	c.LineTo(
+		x+math.Cos(valueAngle-angleWidth/2)*(radius+length),
+		y+math.Sin(valueAngle-angleWidth/2)*(radius+length),
+	)
+	c.ClosePath()
+	c.StrokePreserve()
+	c.SetColor(bulbColor)
+	c.Fill()
+
+	c.MoveTo(
+		x+math.Cos(valueAngle)*(radius-length),
+		y+math.Sin(valueAngle)*(radius-length),
+	)
+	c.LineTo(
+		x+math.Cos(valueAngle)*(radius+length),
+		y+math.Sin(valueAngle)*(radius+length),
+	)
+	c.LineTo(
+		x+math.Cos(valueAngle-angleWidth/2)*(radius+length),
+		y+math.Sin(valueAngle-angleWidth/2)*(radius+length),
+	)
+	c.ClosePath()
+	c.SetColor(color.RGBA{0, 0, 0, 50})
+	c.Fill()
+	c.Pop()
 }
--- Canvas/util/GetColor.go
+++ Canvas/util/GetColor.go
@@ -4,11 +4,31 @@ import (
 	"image/color"
 )
 
-func GetColor(i1, i2 int, g func(int) (float64, color.Color), v float64) color.Color {
+func InterpolateValue(i1, i2 int, g func(int) (float64, color.Color), v float64) color.Color {
 	value0, color0 := g(i1)
 	value1, color1 := g(i2)
 
 	p := (v - value0) / (value1 - value0)
 
 	return Interpolate(color1, color0, p)
+}
+
+func GetColor(v float64, g func(int) (float64, color.Color), l int) color.Color {
+	loVal, loCol := g(0)
+	hiVal, hiCol := g(l - 1)
+
+	nOnly := func(i int) float64 {
+		v, _ := g(i)
+		return v
+	}
+
+	if v <= loVal {
+		return loCol
+	} else if v >= hiVal {
+		return hiCol
+	} else {
+		lowI := FindLoIndex(v, nOnly, l)
+		nColor := InterpolateValue(lowI, lowI+1, g, v)
+		return nColor
+	}
 }
