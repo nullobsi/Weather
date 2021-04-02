ff267841f63664958077b568e42a3e8f8b664ffe
nullobsi <me@nullob.si>
2021-04-01 21:21:19 -0700

    add table capture

---
+++ inputs/table.ts
@@ -0,0 +1,36 @@
+import DataInput from "../defs/DataInput.ts";
+import WeatherData from "../defs/WeatherData.ts";
+
+type TableOpts = {
+    url: string
+}
+/*
+ *
+ * table
+ *      tbody
+ *          tr
+ *              th Title
+ *              td Value
+ */
+
+
+
+const getData: DataInput = async function(options) {
+    let opt = options as TableOpts;
+    let response = await fetch(opt.url);
+    let txt = await response.text();
+    let dat:WeatherData = {};
+
+    let r = /<tr><th>(?<title>.+)<\/th><td>(?<value>.+)<\/td><\/tr>/gim;
+    let m;
+    do {
+        m = r.exec(txt);
+        if (m && m.groups) {
+            dat[m.groups.title] = parseFloat(m.groups.value);
+        }
+    } while (m)
+
+    return dat;
+}
+
+export default getData;
