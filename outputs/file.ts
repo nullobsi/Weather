fe3228f4069f9e99a41bbb8c8592dccac7c21f39
nullobsi <me@nullob.si>
2021-04-11 12:24:11 -0700

    file should discard unneeded datapoints

--- outputs/file.ts
+++ outputs/file.ts
@@ -6,24 +6,17 @@ import * as path from "https://deno.land/std@0.76.0/path/mod.ts"
 const output: DataOutput = async (data, opt, datafields, gradients, processed) => {
     let options: FileOutputOpts = <FileOutputOpts>opt;
     let date = new Date(data.date);
-    let fileName = "";
     let archiveFolder = path.join(Deno.cwd(), "archive");
+
+    let ndat = {...data};
+    if (options.removeKeys !== undefined) {
+        options.removeKeys.forEach(k => {
+            delete ndat[k];
+        })
+    }
     if (!(await fs.exists(archiveFolder))) {
         await Deno.mkdir(archiveFolder);
     }
-    if (options.archive) {
-        archiveFolder = path.join(archiveFolder, options.name);
-        if (!(await fs.exists(archiveFolder))) {
-            await Deno.mkdir(archiveFolder);
-        }
-        fileName = getDateString(date, true) + ".json";
-    } else {
-        fileName = options.name + ".json";
-    }
-    fileName = path.join(archiveFolder, fileName);
-    let json = JSON.stringify(data, undefined, options.pretty ? 4 : undefined);
-    await Deno.writeTextFile(fileName, json);
-
     for (let i = 0; i < options.extraFiles.length; i++) {
         let file = options.extraFiles[i];
         let toSave = processed[file.fieldName];
@@ -37,6 +30,25 @@ const output: DataOutput = async (data, opt, datafields, gradients, processed) =
         fileName = path.join(archiveFolder, fileName);
         await Deno.writeFile(fileName, toSave);
     }
+
+
+
+    let fileName = "";
+
+    if (options.archive) {
+        archiveFolder = path.join(archiveFolder, options.name);
+        if (!(await fs.exists(archiveFolder))) {
+            await Deno.mkdir(archiveFolder);
+        }
+        fileName = getDateString(date, true) + ".json";
+    } else {
+        fileName = options.name + ".json";
+    }
+    fileName = path.join(archiveFolder, fileName);
+    let json = JSON.stringify(data, undefined, options.pretty ? 4 : undefined);
+    await Deno.writeTextFile(fileName, json);
+
+
 }
 
 export default output;
@@ -45,5 +57,6 @@ export type FileOutputOpts = {
     pretty: boolean,
     archive: boolean,
     name: string,
-    extraFiles: {fieldName: string, ext: string}[]
+    extraFiles: {fieldName: string, ext: string}[],
+    removeKeys: string[] | undefined
 }
