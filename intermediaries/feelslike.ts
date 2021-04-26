1d134c86c8140f87d8e4308d7c1c96f5c2c4f849
nullobsi <me@nullob.si>
2021-04-26 16:58:59 -0700

    update feels like

--- intermediaries/feelslike.ts
+++ intermediaries/feelslike.ts
@@ -25,29 +25,31 @@ const FeelsLike: Intermediary = async (opts, data) => {
     let W = data[o.windMph];
     let H = data[o.humidity];
     if (T !== undefined && T !== null && W !== undefined && W !== null && H !== undefined && H !== null) {
-        let windChill =
-            35.74
-            + 0.6215*T
-            - 35.75*Math.pow(W, 0.16)
-            + 0.4275*T*Math.pow(W, 0.16);
+        if (T <= 50) {
+            // wind chill
+            let windChill = 35.74
+                + 0.6215*T
+                - 35.75*Math.pow(W, 0.16)
+                + 0.4275*T*Math.pow(W, 0.16);
+            if (windChill <= T)
+                data[o.nFieldName] = windChill;
+            else
+                data[o.nFieldName] = T;
 
-        let heatIndex =
-            c[1]
-            + c[2]*T
-            + c[3]*H
-            + c[4]*T*H
-            + c[5]*Math.pow(T,2)
-            + c[6]*Math.pow(H,2)
-            + c[7]*Math.pow(T,2)*H
-            + c[8]*T*Math.pow(H,2)
-            + c[9]*Math.pow(T,2)*Math.pow(H,2);
-
-        if (T >= 72.56) {
-            data[o.nFieldName] = heatIndex;
+        } else if (T >= 70) {
+            // heat index
+            data[o.nFieldName] =
+                c[1]
+                + c[2]*T
+                + c[3]*H
+                + c[4]*T*H
+                + c[5]*Math.pow(T,2)
+                + c[6]*Math.pow(H,2)
+                + c[7]*Math.pow(T,2)*H
+                + c[8]*T*Math.pow(H,2)
+                + c[9]*Math.pow(T,2)*Math.pow(H,2);
         } else {
-            if (W != 0) {
-
-            }
+            data[o.nFieldName] = T;
         }
     }
 }
