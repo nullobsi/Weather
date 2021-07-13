f61d52791518a4d081d1322b927b0a3eeec50a52
nullobsi <me@nullob.si>
2021-07-12 17:46:31 -0700

    add csv input plugin

---
+++ inputs/csv.ts
@@ -0,0 +1,44 @@
+import DataInput from "../defs/DataInput.ts";
+import Indexed from "../defs/Indexed.ts";
+import WeatherData from "../defs/WeatherData.ts";
+
+
+const getData: DataInput = async function(options) {
+    let opts = options as CsvPerconf;
+    if (opts.runExecutable !== undefined) {
+        let p = Deno.run({
+            cmd: opts.runExecutable,
+            cwd: opts.workingDirectory,
+            stderr: "null",
+            stdin: "null",
+            stdout: "null"
+        });
+        let status = await p.status();
+        if (status.code === 0) {
+            this.console.log("Subprocess completed with code 0");
+        } else {
+            throw new Error("Subprocess failed with code " + status.code);
+        }
+    }
+    let text = await Deno.readTextFile(opts.filePath);
+    let data: WeatherData = {};
+
+    for (let row of text.split("\n")) {
+        let rowSplit = row.trim().split(",");
+        let title = rowSplit.shift();
+        for (let i = 0; i < rowSplit.length; i++) {
+            data[`${title}${i}`] = parseFloat(rowSplit[i]);
+        }
+    }
+
+    return data;
+}
+
+type CsvPerconf = {
+    runExecutable?: string[],
+    filePath: string,
+    workingDirectory?: string
+}
+
+export type {CsvPerconf}
+export default getData;
