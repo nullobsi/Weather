3e36d119d97bdb37eea17427e45fce2c78a912eb
nullobsi <me@nullob.si>
2022-02-01 21:22:57 -0800

    add waves to pipeline list

--- pipelines.ts
+++ pipelines.ts
@@ -4,9 +4,11 @@ import Indexed from "./defs/Indexed.ts";
 import main from "./pipelines/main.ts";
 import windy from "./pipelines/windy.ts";
 import windyAir from "./pipelines/windyAir.ts";
+import waves from "./pipelines/waves.ts";
 
 export const pipelines: Indexed<Pipeline> = {
     main,
     windy,
     windyAir,
+    waves,
 };
