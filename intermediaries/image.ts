8886fcd33537aa96c114575a71a922c5e6072e18
Kayden Tebau <me@nullob.si>
2022-12-09 09:32:10 -0800

    Try fixing image.ts intermediary

--- intermediaries/image.ts
+++ intermediaries/image.ts
@@ -1,6 +1,5 @@
 import Intermediary from "../defs/Intermediary.ts";
 import * as path from "https://deno.land/std@0.76.0/path/mod.ts";
-import image from "../processors/image.ts";
 /*
    Bright
    Calm
@@ -32,25 +31,26 @@ type ImageIOpts = {
 type ImagePickerPerConf = {
     useFor: "solar" | "temp" | "humidity" | "pressure" | "precip" | "wind";
 }
-export type {ImageIOpts, ImagePickerPerConf, Thresholds};
+export type { ImageIOpts, ImagePickerPerConf, Thresholds };
 
-let lastDate = new Date().getDay();
 
 
 const getImg: Intermediary = async function(options, data, gradients, pipeline){
-    let opts = options as ImageIOpts;
-    let t = opts.thresholds
+    const opts = options as ImageIOpts;
+    const t = opts.thresholds;
 
-    let month = new Date().getMonth();
-    let folder = path.join(opts.folder, (month+1).toString());
+    const month = new Date().getMonth();
+    const folder = path.join(opts.folder, (month+1).toString());
 
     // this line written in the typescript zen - do not touch
-    let values = Object
+    // Find imagePicker confs for values and convert to a dictionary object
+    const values = Object
         .fromEntries(pipeline.datafields
             .filter(v => v.perConfig.imagePicker !== undefined)
             .map(v => [(v.perConfig.imagePicker as ImagePickerPerConf).useFor, data[v.fieldName]])) as
         {[K in ImagePickerPerConf["useFor"]]: number};
 
+    // use thresholds n jazz to pickImage on the right folder
     if (values.solar == 0) {
         data.image = await pickImage(folder, "Night");
         return;
@@ -75,7 +75,7 @@ const getImg: Intermediary = async function(options, data, gradients, pipeline){
 
     if (values.pressure < t.stormy[0] || values.precip > t.stormy[1]) {
         data.image = await pickImage(folder, "Stormy");
-        return
+        return;
     }
 
     if (values.humidity > t.humid) {
@@ -84,46 +84,89 @@ const getImg: Intermediary = async function(options, data, gradients, pipeline){
     }
     if (values.solar > t.bright[month]) {
         data.image = await pickImage(folder, "Bright");
-        return
+        return;
     }
 
+    // Otherwise Calm
     data.image = await pickImage(folder, "Calm");
-
 }
 
+// Last seen date
+let lastDate = new Date().getDate();
+
+// Picked images is reset every day
+// For the given folder and subfolder, store the selected image so it
+// doesn't change throughout the day.
+// It should only change if the conditions change, then another consistent
+// image will be chosen.
 let pickedImages: {[x:string]:string | undefined} = {};
-let seenImages: {[x:string]: string | undefined} = {};
+
+// Seen images is never reset until it's full.
+// For the given folder and subfolder, store the used images.
+// This should encourage a shuffle, so that we use every image at least once; and
+// after, reset the seenImages for that folder to reset the cycle.
+let seenImages: {[x:string]: string[] | undefined} = {};
 
 async function pickImage(folder: string, subdir: string): Promise<Uint8Array> {
-    let currentDay = new Date().getDate();
+    const imageFolder = path.normalize(path.join(folder, subdir));
+    // Get day
+    const currentDay = new Date().getDate();
+
+    // If day has not changed, check if we already picked an image
     if (currentDay == lastDate) {
-        let imageFn = pickedImages[subdir];
-        if (imageFn) {
-            return Deno.readFile(path.join(folder, subdir, imageFn));
+        // Read image and return
+        const imageName = pickedImages[imageFolder];
+        if (imageName) {
+            return Deno.readFile(path.join(imageFolder, imageName));
         }
     } else {
+        // Day has changed. Reset and update lastDate
         pickedImages = {};
         lastDate = currentDay;
     }
-    let fileList = Deno.readDir(path.join(folder, subdir));
+
+    // Get list of files
+    const imageList = Deno.readDir(imageFolder);
     let images: string[] = [];
-    for await (let file of fileList) {
+    for await (const file of imageList) {
         images.push(file.name);
     }
+
+    // No dotfiles
     images = images.filter(v => !v.startsWith("."));
-    let imageFn: string;
-    if (images.length != 1) {
-        images = images.filter(v => v != seenImages[subdir]);
-        imageFn = images[Math.floor(Math.random() * images.length)];
+
+    const folderSeenImages = seenImages[imageFolder];
+    // Choose random image, preferably ones we haven't used before. Almost like a shuffle.
+    let imageName: string;
+    if (images.length > 1) {
+        // Filter images by those not already seen
+        const filteredImages = images.filter(v => !folderSeenImages?.includes(v));
+        // We've gone through all the images, reset
+        if (filteredImages.length == 0) {
+            imageName = images[Math.floor(Math.random() * images.length)];
+            seenImages[imageFolder] = [imageName];
+        } else {
+            // Pick image
+            imageName = filteredImages[Math.floor(Math.random() * filteredImages.length)];
+        }
     } else if (images.length == 1) {
-        imageFn = images[0];
+        // If there's only one...
+        imageName = images[0];
     } else {
         throw new Error("No image found.");
     }
-    let imagePath = path.join(folder, subdir, imageFn);
-    pickedImages[subdir] = imageFn;
-    seenImages[subdir] = imageFn;
-    return Deno.readFile(imagePath);
+    // Indicate picked image
+    pickedImages[imageFolder] = imageName;
+
+    // Create entry in seenImages
+    if (folderSeenImages === undefined) {
+        seenImages[imageFolder] = [imageName];
+    } else {
+        folderSeenImages.push(imageName);
+    }
+
+    // Finally, read image file
+    return Deno.readFile(path.join(imageFolder, imageName));
 }
 
 export default getImg;
